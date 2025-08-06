6c6471802e233e1ea16032c1fdfd522a3f38a4d8
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-08-06 12:24:03 +0200

    glm linked

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <GLFW/glfw3.h>
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/basic_file_sink.h"
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/mat4x4.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+
+// src: https://iquilezles.org/articles/palettes/
+glm::vec3 palette( const float t, const glm::vec3 a, const glm::vec3 b, const glm::vec3 c, const glm::vec3 d ) { return a + b*cos(6.283185f*(c*t+d)); }
 
 int main ()
 {
@@ -24,7 +31,10 @@ int main ()
     glfwMakeContextCurrent(window);
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
+        float time = glfwGetTime();
+
+        glm::vec3 bg(palette(time*.1f,glm::vec3(0.5,0.5,0.5),glm::vec3(0.5,0.5,0.5),glm::vec3(2.0,1.0,0.0),glm::vec3(0.5,0.20,0.25)));
+        glClearColor(bg.r, bg.g , bg.b, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         glfwSwapBuffers(window);
@@ -33,4 +43,5 @@ int main ()
 
     spdlog::info("program was closed");
     glfwTerminate();
+    return 0;
 }
