980c0ca37f2f537c72eee253ff453a35e9ac5d2d
andrew-raphael-lukasik <andrew.r.lukasik@gmail.com>
2025-08-06 12:25:46 +0200

    added 3 glfwWindowHint

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,9 @@ int main ()
     }
     glfwSetErrorCallback(error_callback);
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     GLFWwindow* window = glfwCreateWindow(800, 600, "hello opengl", NULL, NULL);
     if (!window) {
         spdlog::error("glfwCreateWindow() failed");
